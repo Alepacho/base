e0d864c99ffc55a2549feaec4b07329a708eb3a8
Danil Fedorov <danildimaf@gmail.com>
2023-11-28 11:58:54 +0300

    add OS info

--- include/base/base.h
+++ include/base/base.h
@@ -2,6 +2,23 @@
 #ifndef BASE_MAIN_H
 #define BASE_MAIN_H
 
+#define OS_WINDOWS NO
+#define OS_MACOS   NO
+#define OS_LINUX   NO
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmacro-redefined"
+
+#if defined(__APPLE__)
+#define OS_MACOS YES
+#elif defined(_WIN64) || defined(_WIN32)
+#define OS_WINDOWS YES
+#elif defined(__linux__)
+#define OS_LINUX YES
+#endif
+
+#pragma clang diagnostic pop
+
 // https://github.com/gnustep/libs-base
 #include <objc/runtime.h>
 
