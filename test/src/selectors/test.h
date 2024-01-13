5cff9060624a4c8eb60ec61ea637c7279731b72b
Danil Fedorov <danildimaf@gmail.com>
2024-01-13 19:06:53 +0300

    update playground

--- test/src/selectors/test.h
+++ test/src/selectors/test.h
@@ -2,6 +2,13 @@
 #include "../test.h"
 
 @interface TestSelectors : BaseObject <Test> {
+	Selector* selFoo;
 }
+
+- (id)init;
+- (void)dealloc;
+
+- (Selector*)selFoo;
+- (id)setSelFo:(Selector*)newSelFoo;
 
 @end
--- test/src/selectors/test.m
+++ test/src/selectors/test.m
@@ -1,6 +1,49 @@
 #include "test.h"
 
+static TestSelectors* test;
+
+@interface Foo : BaseObject
+
+- (void)func;
+
+@end
+
+@implementation Foo
+
+- (id)init {
+	if (self = [super init]) {
+		[test setSelFo:[[Selector new] setObject:self
+									 setSelector:@selector(func)]];
+	}
+	return self;
+}
+
+- (void)func {
+	[System debug:"foo"];
+}
+
+@end
+
 @implementation TestSelectors
+
+- (id)init {
+	if (self = [super init]) {
+		selFoo = nil;
+	}
+	return self;
+}
+- (void)dealloc {
+	[super dealloc];
+}
+
+- (Selector*)selFoo {
+	return selFoo;
+}
+
+- (id)setSelFo:(Selector*)newSelFoo {
+	selFoo = newSelFoo;
+	return self;
+}
 
 - (void)callback2 {
 	[System debug:"Callback 2!"];
@@ -11,9 +54,21 @@
 }
 
 + (void)execute {
+	test = [TestSelectors new];
+	Foo* foo = [Foo new];
+
 	[System debug:"Selector test begin"];
 	[Selector perform:@selector(callback1) object:self];
-	[Selector perform:@selector(callback2) object:self];
+
+	// @try {
+	// 	[Selector perform:@selector(callback2) object:self];
+	// } @catch (Exception* ex) {
+	// 	[System debug:"Got exception: %s", [ex message]];
+	// }
+
+	[[test selFoo] perform];
+	[foo func];
+
 	[System debug:"Selector test end"];
 }
 
